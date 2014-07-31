9c575821baf5e660d1f7a4f28dc1a300f41398bd
Alex Balan <ammaaim@mail.com>
2014-07-31 16:59:06 +0300

    Create adc.c

---
+++ adc.c
@@ -0,0 +1,26 @@
+
+#include <avr/io.h>
+
+void adc_init() {
+	/* 16Mhz/128 = 125Khz the ADC reference clock */
+	ADCSRA |= (_BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0));
+	/* Voltage reference from Avcc (5v) */
+	ADMUX |= _BV(REFS0);
+	/* Turn on ADC */
+	ADCSRA |= _BV(ADEN);
+	/* Do an initial conversion because this one is the slowest
+	and to ensure that everything is up and running */
+	ADCSRA |= _BV(ADSC);
+}
+
+int adc_read(char ch) {
+	/* Clear the older channel that was read */
+	ADMUX &= 0xF0;
+	/* Defines the new ADC channel to be read */
+	ADMUX |= ch;
+	/* Starts a new conversion */
+	ADCSRA |= _BV(ADSC);
+	/* Wait until the conversion is done */
+	while(ADCSRA & _BV(ADSC));
+	return ADCW;
+}
